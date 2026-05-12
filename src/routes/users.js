a08c8332b6bcdc276a692ce2cf7354a7323c5f61
Issah Nipulo <bed-com-01-20@unima.ac.mw>
2026-05-12 15:37:54 +0200

    updating code

--- src/routes/users.js
+++ src/routes/users.js
@@ -50,10 +50,7 @@ router.put('/profile', protect, async (req, res) => {
         learningStyle: user.learningStyle
       }
     });
-  } catch (error) {
-    console.error('Update profile error:', error);
-    res.status(500).json({ message: error.message });
-  }
+ 
 });
 
 // @route   PUT /api/users/change-password
