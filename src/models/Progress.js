3f2759cd3235168accb5f1fbe4fba57b06695411
Bed-Com-61-22 <bed-com-61-22@unima.ac.mw>
2026-05-22 18:47:46 +0200

    add Progress schema with basic fields

---
+++ src/models/Progress.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const progressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+    
