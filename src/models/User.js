b03cc33794faacdd614a76a1351602347b430bbf
Bed-Com-61-22 <bed-com-61-22@unima.ac.mw>
2026-05-27 21:15:39 +0200

    userpassword

--- src/models/User.js
+++ src/models/User.js
@@ -13,10 +13,12 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpires: Date
 });
 
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 
-userSchema.methods.comparePassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
